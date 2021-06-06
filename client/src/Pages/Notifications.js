e904fff8c3c0ac15c411abeb6a5612841e26c233
Zaino <hello@darrin.dev>
2021-06-06 12:51:09 -0700

    update account container for profile, payments, notifictions, settings

--- client/src/Pages/Notifications.js
+++ client/src/Pages/Notifications.js
@@ -1,18 +1,244 @@
-import React from "react";
+// react 
+import { Fragment, useState } from 'react';
+import { useLocation } from "react-router-dom";
 
-function Notifications() {
-  return (
-    <div>
-      <h1>Notifications Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
-  );
+
+// Account base components 
+import BrandWhite from "../components/BrandWhite";
+import Avatar from "../components/Avatar";
+import ProfileComponent from '../components/ProfileComponent';
+import FooterAccount from "../components/FooterAccount";
+
+// tailwind 
+import {  Dialog, Menu, Transition } from '@headlessui/react';
+import {
+  BellIcon,
+  ChartBarIcon,
+  MenuAlt2Icon,
+  UserCircleIcon,
+  CurrencyDollarIcon,
+  CogIcon,
+  XIcon
+} from '@heroicons/react/outline';
+
+
+const navigation = [
+  
+  { name: 'Profile', location: '/account/profile', icon: UserCircleIcon, current: true },
+  { name: 'Payments', location: '/account/payments', icon: ChartBarIcon, current: false },
+  { name: 'Notifications', location: '/account/notifications', icon: BellIcon, current: false },
+  { name: 'Settings', location: '/account/settings', icon: CogIcon, current: false },
+]
+const userNavigation = [
+  { name: 'Profile', location: '/account/profile' },
+  { name: 'Payments', location: '/account/payments' },
+  { name: 'Notifications', location: '/account/notifications' },
+  { name: 'Settings', location: '/account/settings' },
+  { name: 'Log Out', location: '/logout' },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
 }
 
-export default Notifications;
+export default function Notifications() {
+  const location = useLocation();
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  return (
+    <div className="h-screen flex overflow-hidden bg-gray-100">
+      <Transition.Root show={sidebarOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          static
+          className="fixed inset-0 flex z-40 md:hidden"
+          open={sidebarOpen}
+          onClose={setSidebarOpen}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity ease-linear duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Dialog.Overlay className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+          </Transition.Child>
+          <Transition.Child
+            as={Fragment}
+            enter="transition ease-in-out duration-300 transform"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transition ease-in-out duration-300 transform"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <div className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-indigo-900">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-in-out duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="ease-in-out duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="absolute top-0 right-0 -mr-12 pt-2">
+                  <button
+                    type="button"
+                    className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    onClick={() => setSidebarOpen(false)}
+                  >
+                    <span className="sr-only">Close sidebar</span>
+                    <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                  </button>
+                </div>
+              </Transition.Child>
+              {/* Mobile Nav */}
+              <div className="flex-shrink-0 flex items-center px-4">
+                <BrandWhite />
+              </div>
+              <div className="mt-5 flex-1 h-0 overflow-y-auto">
+                <nav className="px-2 space-y-1">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.location}
+                      className={classNames(
+                        item.current ? 'bg-indigo-800 text-white' : 'text-indigo-100 hover:bg-indigo-600',
+                        'group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                      )}
+                    >
+                      <item.icon className="mr-4 flex-shrink-0 h-6 w-6 text-indigo-300" aria-hidden="true" />
+                      {item.name}
+                    </a>
+                  ))}
+                </nav>
+              </div>
+            </div>
+          </Transition.Child>
+          <div className="flex-shrink-0 w-14" aria-hidden="true">
+            {/* Dummy element to force sidebar to shrink to fit close icon */}
+          </div>
+        </Dialog>
+      </Transition.Root>
+
+      {/* Static sidebar for desktop */}
+      <div className="hidden bg-indigo-900 md:flex md:flex-shrink-0">
+        <div className="flex flex-col w-64">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex flex-col flex-grow pt-5 pb-4 overflow-y-auto">
+            <div className="flex items-center flex-shrink-0 px-4">
+              <BrandWhite />
+            </div>
+            <div className="mt-5 flex-1 flex flex-col">
+              <nav className="flex-1 px-2 space-y-1">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.location}
+                    className={classNames(
+                      item.current ? 'bg-indigo-700 text-white' : 'text-indigo-100 hover:bg-indigo-300',
+                      'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                    )}
+                  >
+                    <item.icon className="mr-3 flex-shrink-0 h-6 w-6 text-indigo-300" aria-hidden="true" />
+                    {item.name}
+                  </a>
+                ))}
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col w-0 flex-1 overflow-hidden">
+        <div className="relative z-10 flex-shrink-0 flex h-16 bg-white shadow">
+          <button
+            type="button"
+            className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+            onClick={() => setSidebarOpen(true)}
+          >
+            <span className="sr-only">Open sidebar</span>
+            <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+          <div className="flex-1 px-4 flex justify-between">
+            <div className="flex-1 flex">
+               {/* Placeholder for Search form  */}
+            </div>
+            <div className="ml-4 flex items-center md:ml-6">
+              <button className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <span className="sr-only">View notifications</span>
+                <a href="/account/notifications"><BellIcon className="h-6 w-6" aria-hidden="true" /></a>
+                
+              </button>
+
+              {/* Profile dropdown */}
+              <Menu as="div" className="ml-3 relative">
+                {({ open }) => (
+                  <>
+                    <div>
+                      <Avatar />
+                    </div>
+                    <Transition
+                      show={open}
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items
+                        static
+                        className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                      >
+                        {userNavigation.map((item) => (
+                          <Menu.Item key={item.name}>
+                            {({ active }) => (
+                              <a
+                                href={item.location}
+                                className={classNames(
+                                  active ? 'bg-gray-100' : '',
+                                  'block px-4 py-2 text-sm text-gray-700'
+                                )}
+                              >
+                                {item.name}
+                              </a>
+                            )}
+                          </Menu.Item>
+                        ))}
+                      </Menu.Items>
+                    </Transition>
+                  </>
+                )}
+              </Menu>
+            </div>
+          </div>
+        </div>
+
+        <main className="flex-1 relative overflow-y-auto focus:outline-none">
+          <div className="py-6">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+              <h1 className="text-2xl font-semibold text-gray-900">Name of Component</h1>
+             
+            </div>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+              {/* Replace with your content */}
+              <div className="py-4">
+              <ProfileComponent />
+                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                
+              </div>
+              {/* /End replace */}
+             
+            </div>
+          </div>
+        </main>
+        <FooterAccount />
+      </div>
+    </div>
+  )
+}
--- client/src/Pages/Profile.js
+++ client/src/Pages/Profile.js
@@ -1,14 +1,16 @@
+// react 
 import { Fragment, useState } from 'react';
-import NavbarIn from "../components/NavbarIn";
-//import NavbarOut from "../components/NavbarOut";
-import Authentication from "../components/Authentication";
-import Footer from "../components/Footer";
-import {  Dialog, Menu, Popover, Transition } from '@headlessui/react';
+import { useLocation } from "react-router-dom";
 
 
-import { Link, useLocation } from "react-router-dom";
-import Brand from "../components/Brand";
-import Form1 from "../components/Form1";
+// Account base components 
+import BrandWhite from "../components/BrandWhite";
+import Avatar from "../components/Avatar";
+import ProfileComponent from '../components/ProfileComponent';
+import FooterAccount from "../components/FooterAccount";
+
+// tailwind 
+import {  Dialog, Menu, Transition } from '@headlessui/react';
 import {
   BellIcon,
   ChartBarIcon,
@@ -17,21 +19,22 @@ import {
   CurrencyDollarIcon,
   CogIcon,
   XIcon
-} from '@heroicons/react/outline'
-import { SearchIcon } from '@heroicons/react/solid';
-import ProfileComponent from '../components/ProfileComponent';
+} from '@heroicons/react/outline';
+
 
 const navigation = [
   
-  { name: 'Profile', href: '#', icon: UserCircleIcon, current: true },
-  { name: 'Payments', href: '#', icon: ChartBarIcon, current: false },
-  { name: 'Notifications', href: '#', icon: BellIcon, current: false },
-  { name: 'Settings', href: '#', icon: CogIcon, current: false },
+  { name: 'Profile', location: '/account/profile', icon: UserCircleIcon, current: true },
+  { name: 'Payments', location: '/account/payments', icon: ChartBarIcon, current: false },
+  { name: 'Notifications', location: '/account/notifications', icon: BellIcon, current: false },
+  { name: 'Settings', location: '/account/settings', icon: CogIcon, current: false },
 ]
 const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Foo', href: '#' },
-  { name: 'Sign out', href: '#' },
+  { name: 'Profile', location: '/account/profile' },
+  { name: 'Payments', location: '/account/payments' },
+  { name: 'Notifications', location: '/account/notifications' },
+  { name: 'Settings', location: '/account/settings' },
+  { name: 'Log Out', location: '/logout' },
 ]
 
 function classNames(...classes) {
@@ -72,7 +75,7 @@ export default function Profile() {
             leaveFrom="translate-x-0"
             leaveTo="-translate-x-full"
           >
-            <div className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-indigo-700">
+            <div className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-indigo-900">
               <Transition.Child
                 as={Fragment}
                 enter="ease-in-out duration-300"
@@ -93,25 +96,16 @@ export default function Profile() {
                   </button>
                 </div>
               </Transition.Child>
+              {/* Mobile Nav */}
               <div className="flex-shrink-0 flex items-center px-4">
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-300-mark-white-text.svg"
-                  alt="Workflow"
-                />
+                <BrandWhite />
               </div>
               <div className="mt-5 flex-1 h-0 overflow-y-auto">
                 <nav className="px-2 space-y-1">
-                  <div>
-                    this is navbar collapsed
-                    <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
-          Home
-        </Link>
-                  </div>
                   {navigation.map((item) => (
                     <a
                       key={item.name}
-                      href={item.href}
+                      href={item.location}
                       className={classNames(
                         item.current ? 'bg-indigo-800 text-white' : 'text-indigo-100 hover:bg-indigo-600',
                         'group flex items-center px-2 py-2 text-base font-medium rounded-md'
@@ -132,21 +126,21 @@ export default function Profile() {
       </Transition.Root>
 
       {/* Static sidebar for desktop */}
-      <div className="hidden bg-indigo-700 md:flex md:flex-shrink-0">
+      <div className="hidden bg-indigo-900 md:flex md:flex-shrink-0">
         <div className="flex flex-col w-64">
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex flex-col flex-grow pt-5 pb-4 overflow-y-auto">
             <div className="flex items-center flex-shrink-0 px-4">
-              <Brand />
+              <BrandWhite />
             </div>
             <div className="mt-5 flex-1 flex flex-col">
               <nav className="flex-1 px-2 space-y-1">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
-                    href={item.href}
+                    href={item.location}
                     className={classNames(
-                      item.current ? 'bg-indigo-800 text-white' : 'text-indigo-100 hover:bg-indigo-600',
+                      item.current ? 'bg-indigo-700 text-white' : 'text-indigo-100 hover:bg-indigo-300',
                       'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
                     )}
                   >
@@ -171,28 +165,13 @@ export default function Profile() {
           </button>
           <div className="flex-1 px-4 flex justify-between">
             <div className="flex-1 flex">
-              <form className="w-full flex md:ml-0" action="#" method="GET">
-                <label htmlFor="search_field" className="sr-only">
-                  Search
-                </label>
-                <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                  <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                    <SearchIcon className="h-5 w-5" aria-hidden="true" />
-                  </div>
-                  <input
-                    id="search_field"
-                    className="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
-                    placeholder="Search"
-                    type="search"
-                    name="search"
-                  />
-                </div>
-              </form>
+               {/* Placeholder for Search form  */}
             </div>
             <div className="ml-4 flex items-center md:ml-6">
               <button className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 <span className="sr-only">View notifications</span>
-                <BellIcon className="h-6 w-6" aria-hidden="true" />
+                <a href="/account/notifications"><BellIcon className="h-6 w-6" aria-hidden="true" /></a>
+                
               </button>
 
               {/* Profile dropdown */}
@@ -200,14 +179,7 @@ export default function Profile() {
                 {({ open }) => (
                   <>
                     <div>
-                      <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </Menu.Button>
+                      <Avatar />
                     </div>
                     <Transition
                       show={open}
@@ -227,7 +199,7 @@ export default function Profile() {
                           <Menu.Item key={item.name}>
                             {({ active }) => (
                               <a
-                                href={item.href}
+                                href={item.location}
                                 className={classNames(
                                   active ? 'bg-gray-100' : '',
                                   'block px-4 py-2 text-sm text-gray-700'
@@ -251,20 +223,21 @@ export default function Profile() {
           <div className="py-6">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
               <h1 className="text-2xl font-semibold text-gray-900">Name of Component</h1>
-              <ProfileComponent />
+             
             </div>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
               {/* Replace with your content */}
-              Component here
-             
-              
               <div className="py-4">
+              <ProfileComponent />
                 <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                
               </div>
               {/* /End replace */}
+             
             </div>
           </div>
         </main>
+        <FooterAccount />
       </div>
     </div>
   )
